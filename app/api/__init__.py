7fb6ca1d90ba6531ac41e8e72805f305618ee885
Henry Xiang <hxxiang@ucdavis.edu>
2021-08-06 20:42:38 -0600

    updated requirements.txt

--- app/api/__init__.py
+++
@@ -1,5 +0,0 @@
-from config import api
-
-from .student import StudentController
-
-api.add_resource(StudentController, '/api/student/<int:id>', '/api/students')
--- install-deps.sh
+++ install-deps.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-pip install autopep8 bcrypt flask flask-restful sqlalchemy gunicorn faker mysql-connector-python
+pip install \
+    autopep8 \
+    bcrypt \
+    flask \
+    flask-restful \
+    sqlalchemy \
+    gunicorn \
+    faker \
+    mysql-connector-python \
+    ipython
 
