302d266dad7782b063fc1488f3a81d8f25d09f35
Henry Xiang <hxxiang@ucdavis.edu>
2021-08-06 22:07:21 -0600

    updated app/demo.py

--- app/demo.py
+++ app/demo.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("Flask Demo")
+
 
 @app.route("/", methods=['GET'])
 def demo():
     return "<h1>Hello, world!</h1>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
